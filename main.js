75ab892e3ec0526a1344fb8974a0aa8fd8e726a5
Jimmy Zhang <jzhang621@gmail.com>
2018-10-21 11:26:06 -0700

    Rookie game

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 
 $( document ).ready(function() {
-	var url = "https://stats.nba.com/stats/leaguedashplayerstats?College=&Conference=&Country=&DateFrom=&DateTo=&Division=&DraftPick=&DraftYear=&GameScope=&GameSegment=&Height=&LastNGames=0&LeagueID=00&Location=&MeasureType=Base&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PaceAdjust=N&PerMode=Totals&Period=0&PlayerExperience=&PlayerPosition=&PlusMinus=N&Rank=N&Season=2017-18&SeasonSegment=&SeasonType=Regular+Season&ShotClockRange=&StarterBench=&TeamID=0&VsConference=&VsDivision=&Weight=&callback=?"
-	var rodger_players = ["Lonzo Ball", "Jayson Tatum", "De'Aaron Fox", "Ben Simmons", "Kyle Kuzma"]
-	var jimmy_players = ["Malik Monk", "Donovan Mitchell", "Josh Jackson", "Dennis Smith Jr.", "Markelle Fultz"]
+	var url = "https://stats.nba.com/stats/leaguedashplayerstats?College=&Conference=&Country=&DateFrom=&DateTo=&Division=&DraftPick=&DraftYear=&GameScope=&GameSegment=&Height=&LastNGames=0&LeagueID=00&Location=&MeasureType=Base&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PaceAdjust=N&PerMode=Totals&Period=0&PlayerExperience=&PlayerPosition=&PlusMinus=N&Rank=N&Season=2018-19&SeasonSegment=&SeasonType=Regular+Season&ShotClockRange=&StarterBench=&TeamID=0&VsConference=&VsDivision=&Weight=&callback=?"
+	var rodger_players = ["Trae Young", "Collin Sexton", "Marvin Bagley III", "Wendell Carter Jr.", "Shai Gilgeous-Alexander"];
+	var jimmy_players = ["Deandre Ayton", "Luka Doncic", "Kevin Knox", "Miles Bridges", "Jaren Jackson Jr."];
 
 	$.getJSON(url,  // url
 	    function (data) {  // success callback
